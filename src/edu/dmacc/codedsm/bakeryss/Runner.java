da744d538bcfb6dabb7e28c0cf1ffdc67d56643f
Nate Buwalda <nabuwalda@dmacc.edu>
2019-03-31 09:18:21 -0500

    Initial commit

---
+++ src/edu/dmacc/codedsm/bakeryss/Runner.java
@@ -0,0 +1,20 @@
+package edu.dmacc.codedsm.bakeryss;
+
+import java.util.Arrays;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Bakery natesBakery = new Bakery();
+        System.out.println("natesBakery = " + natesBakery);
+
+        Bakery pamsBakery =
+            new Bakery(Arrays.asList(),
+                    Arrays.asList("Pam"),
+                    Arrays.asList(),
+                    100000000.0d,
+                    false);
+        System.out.println("pamsBakery = " + pamsBakery);
+    }
+
+}
